a0d4006aed7613cfbfe215713f16b1f345897a05
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-12 17:06:21 +0200

    verify that all and my books links are visible

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -15,3 +15,31 @@ test('Verify that "Login" link is visible', async ({ page }) => {
     const isLoginLinkVisible = await loginLink.isVisible();
     expect(isLoginLinkVisible).toBe(true);
 });
+
+test('Verify that "Register" link is visible', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+    await page.waitForSelector('nav.navbar');
+    const registerLink = await page.$('a[href="/register"]');
+    const isRegisterLinkVisible = await registerLink.isVisible();
+    expect(isRegisterLinkVisible).toBe(true);
+});
+
+test('Verify that "All Books" link is visible after successful login', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+    const allBooksLink = await page.$('a[href="/catalog"]');
+    const isAllBooksLinkVisible = await allBooksLink.isVisible();
+    expect(isAllBooksLinkVisible).toBe(true);
+});
+
+test('Verify that "My Books" link is visible after successful login', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+    const myBooksLink = await page.$('a[href="/profile"]');
+    const isMyBooksLinkVisible = await myBooksLink.isVisible();
+    expect(isMyBooksLinkVisible).toBe(true);
+});
