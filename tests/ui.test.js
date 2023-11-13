68ab840139b7695cc844cbe58a284d31aae40ae3
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-13 09:28:24 +0200

    verify login with empty email or password

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -86,3 +86,33 @@ test('Verify login with empty fields', async ({ page }) => {
     await page.$('a[href="/login"]');
     expect(page.url()).toBe('http://localhost:3000/login');
 });
+
+test('Verify login with empty email', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="password"]', '123456');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$('a[href="/login"]');
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
+
+test('Verify login with empty password', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.click('input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$('a[href="/login"]');
+    expect(page.url()).toBe('http://localhost:3000/login');
+});
