894a9cbcbd3af91c8b6a9a524fae159091b8458c
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-13 13:01:15 +0200

    verify that books are displayed

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -303,3 +303,73 @@ test('Add book with empty description field', async ({ page }) => {
     expect(page.url()).toBe('http://localhost:3000/create');
 });
 
+test('Add book with empty image field', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+
+    await Promise.all([
+        page.click('input[type="submit"]'),
+        page.waitForURL('http://localhost:3000/catalog')
+    ]);
+
+    await page.click('a[href="/create"]');
+    await page.waitForSelector('#create-form');
+    await page.fill('#title', 'Verry good book');
+    await page.fill('#description', 'https://example.com/book-image.jmg');
+    await page.selectOption('#type', 'Fiction');
+    await page.click('#create-form input[type="submit"]');
+
+    page.on('dialog', async dialog => {
+        expect(dialog.type()).toContain('alert');
+        expect(dialog.message()).toContain('All fields are required!');
+        await dialog.accept();
+    });
+
+    await page.$('a[href="/create"]');
+    expect(page.url()).toBe('http://localhost:3000/create');
+});
+
+// All books page tests
+
+test('Verify that all books ae displayed', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+
+    await Promise.all([
+        page.click('input[type="submit"]'),
+        page.waitForURL('http://localhost:3000/catalog')
+    ]);
+
+    await page.waitForSelector('.dashboard');
+    const bookElements = await page.$$('.other-books-list li');
+    expect(bookElements.length).toBeGreaterThan(0);
+});
+
+test('Verify that no books are displayed', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+
+    await Promise.all([
+        page.click('input[type="submit"]'),
+        page.waitForURL('http://localhost:3000/catalog')
+    ]);
+
+    await page.waitForSelector('.dashboard');
+    const noBooksMessage = await page.textContent('.no-books');
+    expect(noBooksMessage).toBe('No books in database!');
+});
+
+// Details page tests
+
+test('Login and navigate to the details page', async ({ page }) => {
+    await page.goto('http://localhost:3000/catalog');
+    await page.waitForSelector('.otherBooks');
+    await page.click('.otherBooks a.button');
+    await page.waitForSelector('.book-information');
+
+    const detailsPageTitle = await page.textContent('.book-information h3');
+    expect(detailsPageTitle).toBe('Think and grow rich');
+});
