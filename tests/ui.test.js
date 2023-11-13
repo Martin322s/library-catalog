347f12932ae80a9177a6df9966bf6054f16b905b
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-13 13:04:03 +0200

    verify that guests and users have access to the details

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -365,7 +365,26 @@ test('Verify that no books are displayed', async ({ page }) => {
 // Details page tests
 
 test('Login and navigate to the details page', async ({ page }) => {
+    await page.goto('http://localhost:3000/login');
+    await page.fill('input[name="email"]', 'peter@abv.bg');
+    await page.fill('input[name="password"]', '123456');
+
+    await Promise.all([
+        page.click('input[type="submit"]'),
+        page.waitForURL('http://localhost:3000/catalog')
+    ]);
+
+    await page.waitForSelector('.otherBooks');
+    await page.click('.otherBooks a.button');
+    await page.waitForSelector('.book-information');
+
+    const detailsPageTitle = await page.textContent('.book-information h3');
+    expect(detailsPageTitle).toBe('Think and grow rich');
+});
+
+test('Navigate to the details page', async ({ page }) => {
     await page.goto('http://localhost:3000/catalog');
+
     await page.waitForSelector('.otherBooks');
     await page.click('.otherBooks a.button');
     await page.waitForSelector('.book-information');
